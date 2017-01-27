2306ce7612912e7e420d4adec2a0774dab2e8de2
Katherine Shiro <katie.shiro@gmail.com>
2017-01-26 20:33:12 -0600

    adds jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require('mysql');
 var password = require('./password.js');
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function(err) {
     if (err) throw err;

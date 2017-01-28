b5967f9c6a3eb12c8dcddd20cab86641fe70a94b
Katherine Shiro <katie.shiro@gmail.com>
2017-01-28 09:49:26 -0600

    pw

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "shiro210130",
+        password: "",
         database: "burgers_db"
     });
 }
